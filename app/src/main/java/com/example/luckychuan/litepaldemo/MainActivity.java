6de0a7ebaf5ac6d98d2c557ecb966c0709d5b0b3
Luckychuan <luckychuan96@163.com>
2017-03-09 16:55:16 +0800

    init db

--- app/src/main/java/com/example/luckychuan/litepaldemo/MainActivity.java
+++ app/src/main/java/com/example/luckychuan/litepaldemo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.luckychuan.litepaldemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.tablemanager.Connector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //创建数据库的按钮
+        ((Button)findViewById(R.id.create_db_btn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Connector.getDatabase();
+            }
+        });
+
     }
 }
